159edd24c9da118c1e7bfb15652edd34652c993e
Tiara Salsabila <tiarasalsa1203@gmail.com>
2024-11-22 20:46:47 +0700

    test commit

--- src/app/dashboard-admin/page.tsx
+++ src/app/dashboard-admin/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
       <div className="flex justify-between top-0 left-0 sticky bg-[#1684A7]">
         <Hamburger toggled={isOpen} toggle={setOpen} size={18} color="#000" />
         <p>hello</p>
+        <p>word</p>
       </div>
       <div></div>
     </>
