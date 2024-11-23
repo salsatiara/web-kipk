68930046415fac1c1c10105e4343cd1fa5501282
Azizah Fakhriyyah Zahra <azizahzahra753@gmail.com>
2024-11-23 20:46:24 +0700

    membuat side navi

--- src/app/dashboard-admin/page.tsx
+++ src/app/dashboard-admin/page.tsx
@@ -3,11 +3,16 @@
 import { Fade as Hamburger } from "hamburger-react";
 import { useState } from "react";
 import {
+  BsBook,
+  BsBoxArrowRight,
   BsCardChecklist,
   BsCircleFill,
   BsClipboardXFill,
   BsFillPeopleFill,
+  BsJournal,
+  BsJournalText,
   BsPersonCircle,
+  BsPlusSlashMinus,
 } from "react-icons/bs";
 
 export default function Home() {
@@ -15,11 +20,36 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex top-0 left-0 sticky bg-[#1684A7]">
+      <div className="flex top-0 left-0 z-40 sticky bg-[#1684A7]">
         <Hamburger toggled={isOpen} toggle={setOpen} size={18} color="#000" />
         <div className="flex w-full justify-between items-center ml-4 mr-6 text-sm font-semibold">
           <p className="text-black">KIP-Kuliah</p>
           <BsPersonCircle size={24} color="#000" />
+        </div>
+      </div>
+      <div
+        className={`flex flex-col top-12 left-0 fixed z-30 bg-[#CFE4EB] text-black duration-200 ${
+          isOpen ? "translate-x-0" : "-translate-x-56"
+        }`}
+      >
+        <div className="px-6 py-4 text-sm flex items-center border-b border-[#A3A3A3]">
+          <BsBook size={24} color="#000" />
+          <p className="ml-2">Data Calon Penerima</p>
+        </div>
+        <div className="px-6 py-4 text-sm flex items-center border-b border-[#A3A3A3]">
+          <BsJournalText size={24} color="#000" />
+          <p className="ml-2">Data Kriteria</p>
+        </div>
+        <div className="px-6 py-4 text-sm flex items-center border-b border-[#A3A3A3]">
+          <BsPlusSlashMinus
+            className="border-[1.7px] rounded border-black p-1 stroke-[1px]"
+            size={24}
+            color="#000"
+          />
+          <p className="ml-2">Perhitungan</p>
+        </div>
+        <div className="px-6 py-4 mt-10 ml-auto">
+          <BsBoxArrowRight size={24} />
         </div>
       </div>
       <div className="bg-white h-screen text-black flex flex-col items-center">
