088bf65a5cbde2e7d515423866ccc93c62924e2f
Tiara Salsabila <tiarasalsa1203@gmail.com>
2024-12-02 19:42:18 +0700

    membuat api membaca alternatif

---
+++ src/app/api/alternatif/route.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const data = await prisma.alternatif.findMany();
+
+    return Response.json(
+      {
+        status: "Success",
+        message: "Data found successfully",
+        data: data,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {}
+}
