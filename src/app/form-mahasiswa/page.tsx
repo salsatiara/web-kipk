6674492ee77223f601d8aeae6978bcef21c3755c
Tiara Salsabila <tiarasalsa1203@gmail.com>
2024-12-02 19:40:44 +0700

    membuat select option

--- src/app/form-mahasiswa/page.tsx
+++ src/app/form-mahasiswa/page.tsx
@@ -11,6 +11,15 @@ import {
 } from "react-icons/bs";
 export default function Form() {
   const [isOpen, setOpen] = useState(false);
+
+  const [nisn, setNisn] = useState("");
+  const [nama, setNama] = useState("");
+  const [penghasilan, setPenghasilan] = useState("");
+  const [tanggungan, setTanggungan] = useState("");
+  const [nilai, setNilai] = useState("");
+  const [rumah, setRumah] = useState("");
+  const [listrik, setListrik] = useState("");
+
   return (
     <>
       <div className="flex top-0 left-0 z-40 sticky bg-[#1684A7]">
@@ -75,53 +84,123 @@ export default function Form() {
             </div>
             <div className="flex flex-col mt-4 text-xs">
               <label htmlFor="penghasilan">Penghasilan Orang Tua</label>
-              <input
-                type="number"
+              <select
                 name="penghasilan"
                 id="penghasilan"
-                placeholder="Rp 2.000.000"
                 className="bg-[#D9D9D9] px-3 py-2"
-              />
+              >
+                <option value="1" className="bg-white">
+                  ≥ Rp 5.000.001
+                </option>
+                <option value="2" className="bg-white">
+                  Rp 3.500.001 - Rp 5.000.000
+                </option>
+                <option value="3" className="bg-white">
+                  Rp 2.500.001 - Rp 3.500.000
+                </option>
+                <option value="4" className="bg-white">
+                  Rp 1.500.001 - Rp 2.500.000
+                </option>
+                <option value="5" className="bg-white">
+                  ≤ Rp 1.500.000
+                </option>
+              </select>
             </div>
             <div className="flex flex-col mt-4 text-xs">
               <label htmlFor="tanggungan">Jumlah Tanggungan Keluarga</label>
-              <input
-                type="number"
+              <select
                 name="tanggungan"
                 id="tanggungan"
-                placeholder="3 Orang"
                 className="bg-[#D9D9D9] px-3 py-2"
-              />
+              >
+                <option value="1" className="bg-white">
+                  1 Orang
+                </option>
+                <option value="2" className="bg-white">
+                  2 Orang
+                </option>
+                <option value="3" className="bg-white">
+                  3 Orang
+                </option>
+                <option value="4" className="bg-white">
+                  4 Orang
+                </option>
+                <option value="5" className="bg-white">
+                  &gt; 4 Orang
+                </option>
+              </select>
             </div>
             <div className="flex flex-col mt-4 text-xs">
               <label htmlFor="nilai">Nilai Rata-rata Rapor SMA/SMK</label>
-              <input
-                type="number"
+              <select
                 name="nilai"
                 id="nilai"
-                placeholder="88"
                 className="bg-[#D9D9D9] px-3 py-2"
-              />
+              >
+                <option value="1" className="bg-white">
+                  &lt; 75
+                </option>
+                <option value="2" className="bg-white">
+                  75 - 79,99
+                </option>
+                <option value="3" className="bg-white">
+                  80 - 84,99
+                </option>
+                <option value="4" className="bg-white">
+                  85 - 89,99
+                </option>
+                <option value="5" className="bg-white">
+                  ≥ 90
+                </option>
+              </select>
             </div>
             <div className="flex flex-col mt-4 text-xs">
-              <label htmlFor="aset">Kondisi Rumah dan Aset</label>
-              <input
-                type="text"
-                name="aset"
-                id="aset"
-                placeholder="Rumah milik sendiri, punya motor"
+              <label htmlFor="rumah">Kondisi Rumah dan Aset</label>
+              <select
+                name="rumah"
+                id="rumah"
                 className="bg-[#D9D9D9] px-3 py-2"
-              />
+              >
+                <option value="1" className="bg-white">
+                  Rumah milik sendiri, punya mobil dan motor
+                </option>
+                <option value="2" className="bg-white">
+                  Rumah milik sendiri, punya motor
+                </option>
+                <option value="3" className="bg-white">
+                  Rumah warisan, tidak punya kendaraan
+                </option>
+                <option value="4" className="bg-white">
+                  Mengontrak/Kos, tidak punya kendaraan
+                </option>
+                <option value="5" className="bg-white">
+                  Menumpang (ikut saudara/kerabat), tidak punya kendaraan
+                </option>
+              </select>
             </div>
             <div className="flex flex-col mt-4 text-xs">
               <label htmlFor="listrik">Daya Listrik PLN</label>
-              <input
-                type="number"
+              <select
                 name="listrik"
                 id="listrik"
-                placeholder="900 VA"
                 className="bg-[#D9D9D9] px-3 py-2"
-              />
+              >
+                <option value="1" className="bg-white">
+                  &gt; 2200 VA
+                </option>
+                <option value="2" className="bg-white">
+                  2200VA
+                </option>
+                <option value="3" className="bg-white">
+                  1300 VA
+                </option>
+                <option value="4" className="bg-white">
+                  900 VA
+                </option>
+                <option value="5" className="bg-white">
+                  450 VA
+                </option>
+              </select>
             </div>
             <div className="flex justify-evenly my-6 text-sm">
               <button className="px-5 py-2 bg-[#09A599] text-white font-bold rounded">
