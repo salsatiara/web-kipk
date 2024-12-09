4c1861590891792d44b3be9538e6060524d0b6a4
Tiara Salsabila <tiarasalsa1203@gmail.com>
2024-12-09 20:40:14 +0700

    membuat page login bekerja

--- src/app/login-student/page.tsx
+++ src/app/login-student/page.tsx
@@ -1,7 +1,81 @@
+"use client";
+
+import { AuthContext } from "@/services/storage";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FormEvent, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import jwt from "jsonwebtoken";
+
+interface PayloadToken {
+  id: string;
+  username: string;
+  role: string;
+  nisn: string;
+  iat: number;
+  exp: number;
+}
 
 export default function Home() {
+  const router = useRouter();
+  const auth = useContext(AuthContext);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function refreshToken() {
+    try {
+      const response = await axios.post(
+        "/api/auth/token",
+        {},
+        { withCredentials: true }
+      );
+      console.log(response.data);
+
+      auth.setToken(response.data.accessToken);
+      const decoded =
+        (jwt.decode(response.data.accessToken) as PayloadToken) || null;
+      if (decoded) {
+        auth.setId(decoded.id);
+        auth.setUsername(decoded.username);
+        auth.setRole(decoded.role);
+        auth.setNisn(decoded.nisn);
+        auth.setExpire(decoded.exp);
+        router.push("/dashboard-admin"); // nanti ganti student
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function loginHandler(
+    e: FormEvent,
+    username: string,
+    password: string
+  ) {
+    try {
+      e.preventDefault();
+      await axios.post(
+        "/api/auth/login",
+        { username: username, password: password },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      router.push("/dashboard-admin"); // nanti ganti student
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    refreshToken();
+  }, []);
+
   return (
     <>
       <div className="flex w-full h-screen min-h-min bg-gray-100 text-black justify-center">
@@ -24,17 +98,26 @@ export default function Home() {
             <input
               className="bg-[#E6E6E6] w-full rounded px-3 py-2 text-xs"
               type="text"
+              required
               placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
             <input
               className="bg-[#E6E6E6] w-full rounded px-3 py-2 text-xs mt-8"
               type="password"
+              required
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <p className="text-[#09A599] text-xs text-right mt-2">
               Lupa password?
             </p>
-            <button className="bg-[#09A599] w-full rounded text-white text-xs py-2 mt-4">
+            <button
+              className="bg-[#09A599] w-full rounded text-white text-xs py-2 mt-4"
+              onClick={(e) => loginHandler(e, username, password)}
+            >
               Log in
             </button>
             <Link
@@ -42,6 +125,12 @@ export default function Home() {
               className="text-[#09A599] text-xs text-center mt-4 mx-auto"
             >
               Switch to Admin Login
+            </Link>
+            <Link
+              href="/register-student"
+              className="text-[#09A599] text-xs text-center mt-4 mx-auto"
+            >
+              Don&apos;t Have an Account?
             </Link>
           </form>
         </div>
