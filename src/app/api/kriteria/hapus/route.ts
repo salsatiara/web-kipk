b9a882f6772759750d6b53cd8c7b9cdbc33fe99d
Mashyta Dian Adelia Putri <mashytadianap@gmail.com>
2024-12-10 12:01:56 +0700

    membuat route hapus kriteria

---
+++ src/app/api/kriteria/hapus/route.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function DELETE(request: Request) {
+  try {
+    const formData = await request.formData();
+
+    const id: FormDataEntryValue = (formData.get("id") as string) || "";
+
+    if (id === null || id === "") {
+      return Response.json(
+        {
+          status: "error",
+          message: "id cannot be empty",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const data = await prisma.kriteria.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    return Response.json(
+      {
+        status: "Success",
+        message: `Alternatif with id ${id} has been deleted`,
+        data: data,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return Response.json({
+        status: "Error",
+        error: error.message,
+      });
+    }
+  }
+}
