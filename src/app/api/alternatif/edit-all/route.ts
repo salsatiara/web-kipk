c1ddcf99dc3403f400f40800085728078f1d5d16
Tiara Salsabila <tiarasalsa1203@gmail.com>
2024-12-12 17:15:12 +0700

    membuat route edit all alternatif

---
+++ src/app/api/alternatif/edit-all/route.ts
@@ -0,0 +1,80 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type Alternatif = {
+  id: number;
+  nisn: string;
+  nama: string;
+  penghasilan: number;
+  jmlTanggungan: number;
+  nilai: number;
+  rumah: number;
+  listrik: number;
+  jarakPositif: number;
+  jarakNegatif: number;
+  preferensi: number;
+}[];
+
+export async function POST(request: Request) {
+  try {
+    const formData = await request.formData();
+
+    const alternatif: FormDataEntryValue =
+      (formData.get("alternatif") as string) || "[]";
+
+    if (alternatif === null || alternatif === "") {
+      return Response.json(
+        {
+          status: "error",
+          message: "alternatif cannot be empty",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const matriks: Alternatif = JSON.parse(alternatif);
+
+    const data = await prisma.$transaction(
+      matriks.map((matrik) =>
+        prisma.alternatif.update({
+          where: {
+            id: matrik.id,
+          },
+          data: {
+            nisn: matrik.nisn,
+            nama: matrik.nama,
+            penghasilan: matrik.penghasilan,
+            jmlTanggungan: matrik.jmlTanggungan,
+            nilai: matrik.nilai,
+            rumah: matrik.rumah,
+            listrik: matrik.listrik,
+            jarakPositif: matrik.jarakPositif,
+            jarakNegatif: matrik.jarakNegatif,
+            preferensi: matrik.preferensi,
+          },
+        })
+      )
+    );
+
+    return Response.json(
+      {
+        status: "Success",
+        message: "Data has been updated",
+        data: data,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return Response.json({
+        status: "Error",
+        error: error.message,
+      });
+    }
+  }
+}
