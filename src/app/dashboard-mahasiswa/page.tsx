6f8722d4b8e161166f24d4dbc9157eb85a5c3341
Mashyta Dian Adelia Putri <mashytadianap@gmail.com>
2024-12-16 12:38:28 +0700

    membuat dashboard mahasiswa berfungsi

--- src/app/dashboard-mahasiswa/page.tsx
+++ src/app/dashboard-mahasiswa/page.tsx
@@ -4,14 +4,29 @@ import Navbar from "@/components/Navbar";
 import { AuthContext } from "@/services/storage";
 import axios, { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import jwt from "jsonwebtoken";
-import {
-  BsCardChecklist,
-  BsCircleFill,
-  BsClipboardXFill,
-  BsFillPeopleFill,
-} from "react-icons/bs";
+import Link from "next/link";
+import Logo from "@/images/logo.png";
+import Image from "next/image";
+
+type AlternatifType = {
+  status: string;
+  message: string;
+  data: {
+    id: number;
+    nisn: string;
+    nama: string;
+    penghasilan: number;
+    jmlTanggungan: number;
+    nilai: number;
+    rumah: number;
+    listrik: number;
+    jarakPositif: number;
+    jarakNegatif: number;
+    preferensi: number;
+  }[];
+};
 
 interface PayloadToken {
   id: string;
@@ -23,13 +38,16 @@ interface PayloadToken {
 }
 
 export default function Home() {
-  const axiosToken = axios.create();
   const router = useRouter();
   const auth = useContext(AuthContext);
 
+  const [mahasiswa, setMahasiswa] = useState<AlternatifType["data"]>([]);
+  const [selamat, setSelamat] = useState("");
+  const [pesan, setPesan] = useState("");
+
   async function refreshToken() {
     try {
-      const response = await axiosToken.post(
+      const response = await axios.post(
         "/api/auth/token",
         {},
         { withCredentials: true }
@@ -54,70 +72,95 @@ export default function Home() {
     }
   }
 
+  async function fetchData() {
+    try {
+      const response = await axios.get<AlternatifType>(
+        "/api/alternatif?sort=tertinggi&limit=seleksi"
+      );
+
+      const mahasiswa = response.data.data.filter((item) => {
+        return item.nisn == auth.nisn;
+      });
+      setMahasiswa(mahasiswa);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
     async function fetcher() {
       await refreshToken();
+      await fetchData();
     }
     fetcher();
   }, []);
+
+  useEffect(() => {
+    if (mahasiswa.length > 0) {
+      setSelamat("SELAMAT!!");
+      setPesan("ANDA DINYATAKAN LULUS DALAM SELEKSI BEASISWA KIP-KULIAH");
+    } else {
+      setSelamat("MOHON MAAF");
+      setPesan(
+        "ANDA BELUM DINYATAKAN LULUS DALAM SELEKSI BEASISWA KIP-KULIAH "
+      );
+    }
+  }, [mahasiswa]);
 
   return (
     <>
       <Navbar />
       <div className="bg-white h-screen text-black flex flex-col items-center">
-        <div className="lg:w-1/3 flex flex-col items-center">
-          <div className="bg-[#E6E6E6] w-full rounded-lg mt-14 px-6 py-4">
-            <p className="text-sm">Total Data Calon Penerima Beasiswa</p>
-            <div className="flex items-center mt-1">
-              <BsFillPeopleFill size={18} color="#09A599" />
-              <p className="ml-4 font-semibold">100</p>
+        <div className="w-full flex flex-col items-center">
+          <div className="w-full flex mx-auto justify-center px-4 py-6 bg-[#E6E6E6] rounded">
+            <div className="w-1/3 flex gap-8">
+              <div className="flex items-center">
+                <Image
+                  src={Logo}
+                  className="w-32"
+                  width={512}
+                  height={512}
+                  alt="Logo UNJ"
+                />
+              </div>
+              <div
+                className={`flex flex-col ${
+                  mahasiswa.length < 1 ? "text-[#EB0707]" : "text-[#1684A7]"
+                } font-bold`}
+              >
+                <p className="text-xl">{selamat}</p>
+                <p className="text-sm">{pesan}</p>
+              </div>
             </div>
-            <p className="text-xs text-right">20 data baru</p>
           </div>
-          <div className="bg-[#E6E6E6] w-full rounded-lg mt-14 px-6 py-4">
-            <p className="text-sm">Kriteria Penilaian</p>
-            <div className="flex items-center mt-1">
-              <BsCardChecklist size={18} color="#09A599" />
-              <p className="ml-4 font-semibold">5</p>
-            </div>
-            <p className="text-xs text-right">parameter seleksi</p>
-          </div>
-          <div className="bg-[#E6E6E6] w-full rounded-lg mt-14 px-6 py-4">
-            <p className="text-sm">Status Penilaian</p>
-            <div className="flex items-center mt-1">
-              <BsClipboardXFill size={18} color="#09A599" />
-              <p className="ml-4 font-semibold">83</p>
-            </div>
-            <p className="text-xs text-right">dari 100 mahasiswa</p>
-          </div>
-          <div className="bg-[#E6E6E6] w-full rounded-lg mt-14 px-6 py-4">
-            <p className="text-sm">Aktivitas Terbaru</p>
-            <div className="flex items-center mt-2">
-              <BsCircleFill size={7} color="#09A599" />
-              <p className="text-xs ml-4 ">Penambahan data mahasiswa baru</p>
-              <p className="text-xs ml-auto">1m</p>
-            </div>
-            <div className="flex items-center mt-2">
-              <BsCircleFill size={7} color="#09A599" />
-              <p className="text-xs ml-4 ">Pembaruan kriteria penilaian</p>
-              <p className="text-xs ml-auto">12m</p>
-            </div>
-            <div className="flex items-center mt-2">
-              <BsCircleFill size={7} color="#09A599" />
-              <p className="text-xs ml-4 ">
-                Penghapusan data mahasiswa yang tidak memenuhi syarat
+          <div className="mt-14 w-1/3 flex flex-col text-sm">
+            <p>Nama</p>
+            <div className="bg-[#E6E6E6] rounded-lg px-4 py-3">
+              <p>
+                {mahasiswa?.length < 1
+                  ? "Anda tidak terdaftar"
+                  : mahasiswa[0].nama}
               </p>
-              <p className="text-xs ml-auto">1j</p>
             </div>
-            <div className="flex items-center mt-2">
-              <BsCircleFill size={7} color="#09A599" />
-              <p className="text-xs ml-4 ">Perhitungan batch 4 selesai</p>
-              <p className="text-xs ml-auto">5j</p>
+            <p className="mt-8">NISN</p>
+            <div className="bg-[#E6E6E6] rounded-lg px-4 py-3">
+              <p>{auth.nisn}</p>
             </div>
-            <div className="flex items-center mt-2">
-              <BsCircleFill size={7} color="#09A599" />
-              <p className="text-xs ml-4 ">Perhitungan batch 3 selesai</p>
-              <p className="text-xs ml-auto">12j</p>
+            <div className="flex justify-start gap-4 mt-8">
+              <p className="text-[#EB0707]">*</p>
+              <p className="text-justify">
+                Berdasarkan analisis Sistem Pendukung Keputusan (SPK) dengan
+                metode TOPSIS, Anda berhasil mencapai skor preferensi tertinggi
+                dengan memperlihatkan performa unggul dalam kriteria.
+              </p>
+            </div>
+            <div className="mx-auto mt-8">
+              <Link
+                className="bg-[#09A599] text-white px-3 py-3 rounded"
+                href="/perhitungan-topsis"
+              >
+                Lihat Perhitungan
+              </Link>
             </div>
           </div>
         </div>
